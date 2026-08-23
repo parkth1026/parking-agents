560991bfd296f7831995a434760a5ab930f94d51
parkth1026 <parkth1026@hotmail.com>
2026-08-23 22:29:21 +0800

    delete dev

--- skills/dev/cpu-monitor/scripts/.sys-snapshot.ps1
+++
@@ -1,3 +0,0 @@
-$ErrorActionPreference='SilentlyContinue'
-$samples = (Get-Counter '\Processor(_Total)\% Processor Time','\Process(Idle)\% Processor Time').CounterSamples
-foreach ($s in $samples) { Write-Output ($s.Path + '=' + $s.CookedValue) }
